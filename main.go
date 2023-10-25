232b06b2acefc03dcc566124e70be4f0c63120a8
Emmanuel Ramon <emmanuelrlima@gmail.com>
2023-10-24 21:24:12 -0300

    feat: add opening schema

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/ramown/gopportunites/router"
+import (
+	"github.com/ramown/gopportunites/config"
+	"github.com/ramown/gopportunites/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	// Initialize configs
+	if err := config.Init(); err != nil {
+		logger.Errorf("config initialization erro: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
